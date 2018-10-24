965b27f480f4a029fe9b5a0c2973d1b28a12776c
Nate Radebaugh <nate.radebaugh@outlook.com>
2018-10-23 20:51:44 -0500

    Remove extra reports from general jest config.

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,5 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest"
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  reporters: ["default", "jest-junit"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
