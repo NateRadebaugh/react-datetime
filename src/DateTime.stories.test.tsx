baaf92385a5ba416c1c9a4cb045cedb8d0c707d6
Nate Radebaugh <nradebaugh@bdo.com>
2022-12-29 13:57:27 -0600

    Bump storybook to latest beta 7.0.0-beta.16

--- src/DateTime.stories.test.tsx
+++ src/DateTime.stories.test.tsx
@@ -4,8 +4,19 @@ import { render } from "@testing-library/react";
 import * as stories from "./DateTime.stories";
 
 test.each(
+  // @ts-ignore
   Object.keys(stories).filter((name) => typeof stories[name] === "function")
 )("should render %s story", (story) => {
+  // @ts-ignore
   const StoryComponent = stories[story];
-  render(<StoryComponent />);
+
+  if (
+    "render" in StoryComponent &&
+    typeof StoryComponent.render === "function"
+  ) {
+    const { render: Component, args } = StoryComponent;
+    render(Component(args));
+  } else {
+    render(<StoryComponent {...StoryComponent.args} />);
+  }
 });
